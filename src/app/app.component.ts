5c9ef3e5f7eb4e7e5bb68e435fc0fbf063d4dc0c
saledga <juan.mesa@comware.com.co>
2024-06-26 14:37:06 -0500

    Nuevas dependencias  y cambios en html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ChatComponent } from './components/chat/chat.component';
-import {FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { enviroment } from '../enviroments/enviroment';
+import { ChatService } from './services/chat.service';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +12,9 @@ import {FormsModule } from '@angular/forms'
     RouterOutlet,
     ChatComponent,
     FormsModule,  
+  ],
+  providers: [
+    ChatService,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
